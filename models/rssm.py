aeacb97833c3f273e83561e967ccad69efa59c68
Takata1128 <t2130072@edu.cc.uec.ac.jp>
2022-01-05 04:40:30 +0000

    discrete rssm

--- models/rssm.py
+++ models/rssm.py
@@ -11,6 +11,8 @@ class RecurrentStateSpaceModel(nn.Module):
 
     def __init__(
         self,
+        node_size,
+        embedding_size,
         state_dim,
         action_dim,
         rnn_hidden_dim,
@@ -19,18 +21,32 @@ class RecurrentStateSpaceModel(nn.Module):
         act=F.elu,
     ):
         super(RecurrentStateSpaceModel, self).__init__()
+        self.node_size = node_size
+        self.embedding_size = embedding_size
         self.state_dim = state_dim
         self.action_dim = action_dim
         self.rnn_hidden_dim = rnn_hidden_dim
         self.fc_state_action = nn.Linear(state_dim + action_dim, hidden_dim)
         self.fc_rnn_hidden = nn.Linear(rnn_hidden_dim, hidden_dim)
-        self.fc_state_mean_prior = nn.Linear(hidden_dim, state_dim)
-        self.fc_state_stddev_prior = nn.Linear(hidden_dim, state_dim)
-        self.fc_rnn_hidden_embedded_obs = nn.Linear(rnn_hidden_dim + 1024, hidden_dim)
-        self.fc_state_mean_posterior = nn.Linear(hidden_dim, state_dim)
-        self.fc_state_stddev_posterior = nn.Linear(hidden_dim, state_dim)
+        self.fc_prior = nn.Sequential(
+            nn.Linear(hidden_dim, node_size), act(), nn.Linear(node_size, state_dim)
+        )
+
+        # self.fc_state_mean_prior = nn.Linear(hidden_dim, state_dim)
+        # self.fc_state_stddev_prior = nn.Linear(hidden_dim, state_dim)
+
+        # self.fc_rnn_hidden_embedded_obs = nn.Linear(
+        #     rnn_hidden_dim + embedding_size, hidden_dim
+        # )
+        self.fc_posterior = nn.Sequential(
+            nn.Linear(rnn_hidden_dim + embedding_size, node_size),
+            act(),
+            nn.Linear(node_size, state_dim),
+        )
+        # self.fc_state_mean_posterior = nn.Linear(hidden_dim, state_dim)
+        # self.fc_state_stddev_posterior = nn.Linear(hidden_dim, state_dim)
         self.rnn = nn.GRUCell(hidden_dim, rnn_hidden_dim)
-        self._min_stddev = min_stddev
+        # self._min_stddev = min_stddev
         self.act = act
 
     def forward(self, state, action, rnn_hidden, embedded_next_obs):
@@ -38,8 +54,8 @@ class RecurrentStateSpaceModel(nn.Module):
         h_t+1 = f(h_t,s_t,a_t)
         prior p(s_t+1 | h_t+1) と posterior p(s_t+1 | h_t+1, o_t+1)を返す
         """
-        next_state_prior, rnn_hidden = self.prior(state, action, rnn_hidden)
-        next_state_posterior = self.posterior(rnn_hidden, embedded_next_obs)
+        _, next_state_prior, _ = self.prior(state, action, rnn_hidden)
+        _, next_state_posterior, _ = self.posterior(rnn_hidden, embedded_next_obs)
         return next_state_prior, next_state_posterior
 
     def prior(self, state, action, rnn_hidden):
@@ -50,19 +66,29 @@ class RecurrentStateSpaceModel(nn.Module):
         rnn_hidden = self.rnn(hidden, rnn_hidden)
         hidden = self.act(self.fc_rnn_hidden(rnn_hidden))
 
-        mean = self.fc_state_mean_prior(hidden)
-        stddev = F.softplus(self.fc_state_stddev_prior(hidden)) + self._min_stddev
-        return Normal(mean, stddev), rnn_hidden
+        # mean = self.fc_state_mean_prior(hidden)
+        # stddev = F.softplus(self.fc_state_stddev_prior(hidden)) + self._min_stddev
+        # return Normal(mean, stddev), rnn_hidden
+
+        prior_logit = self.fc_prior(hidden)
+        prior_stoch = get_stoch_state(prior_logit)
+        return prior_logit, prior_stoch, rnn_hidden
 
     def posterior(self, rnn_hidden, embedded_obs):
         """
         posterior q(s_t|h_t,o_t)
         """
-        hidden = self.act(
-            self.fc_rnn_hidden_embedded_obs(
-                torch.cat([rnn_hidden, embedded_obs], dim=1)
-            )
-        )
-        mean = self.fc_state_mean_posterior(hidden)
-        stddev = F.softplus(self.fc_state_stddev_posterior(hidden)) + self._min_stddev
-        return Normal(mean, stddev)
+        x = torch.cat([rnn_hidden, embedded_obs], dim=1)
+        # mean = self.fc_state_mean_posterior(hidden)
+        # stddev = F.softplus(self.fc_state_stddev_posterior(hidden)) + self._min_stddev
+        # return Normal(mean, stddev)
+        posterior_logit = self.fc_posterior(x)
+        posterior_stoch = get_stoch_state(posterior_logit)
+        return posterior_logit, posterior_stoch, rnn_hidden
+
+    def rollout_observation(self, seq_len, obs_embed, action, prev_rssm_state):
+        priors = []
+        posteriors = []
+        for t in range(seq_len):
+            prev_action = action[t]
+            prior_state, posterior_state = self.forward()
